58798a163744d75717a764a9506f5eebc8ec5e71
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-18 18:40:03 +0000

    automatically backed up by learn

--- looping.rb
+++ looping.rb
@@ -1,5 +1,14 @@
 def looping
 	#your code here
+  count = 0
+  loop do 
+    count +=1
+    puts "Wingardium Leviosa"
+    if count >= 5
+      break
+    end
+    
 end
 
 #call your method here
+looping
