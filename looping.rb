ec5dac28b1879ee3b83dc46dae331ba7107d158a
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-18 18:43:10 +0000

    automatically backed up by learn

--- looping.rb
+++ looping.rb
@@ -1,14 +1,9 @@
 def looping
 	#your code here
-  count = 0
-  loop do
-    puts "Wingardium Leviosa"
-    count +=1
-    if count >= 10
-      break
-    end
-  end
-end
 
+  loop do
+  puts "I have found the Time Machine!"
+end
+end
 #call your method here
 looping
